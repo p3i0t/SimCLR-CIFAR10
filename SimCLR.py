9d99fe388cde61de9196a7112911357794cb117d
a-name-not-used <wangxin_0716@me.com>
2020-04-25 15:04:20 +0800

    Initial

--- SimCLR.py
+++ SimCLR.py
@@ -1,3 +1,7 @@
+import hydra
+from omegaconf import DictConfig
+import logging
+
 import numpy as np
 import logging
 from PIL import Image
@@ -7,34 +11,11 @@ import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, Dataset
 import torchvision.transforms as tfs
-from torchvision.datasets import *
-from torchvision.models import *
+from torchvision.datasets import CIFAR10
+from torchvision.models import resnet18, resnet34, resnet50
 from utils import AverageMeter
 
 logger = logging.getLogger(__name__)
-
-tf_tr = tfs.Compose([
-    tfs.RandomResizedCrop(32),
-    tfs.RandomHorizontalFlip(),
-    tfs.ColorJitter(0.5, 0.5, 0.5, 0.5),
-    tfs.ToTensor(),
-    tfs.Normalize(mean=[0.485, 0.456, 0.406],
-                  std=[0.229, 0.224, 0.225])
-])
-
-tf_de = tfs.Compose([
-    tfs.Resize(32),
-    tfs.ToTensor(),
-    tfs.Normalize(mean=[0.485, 0.456, 0.406],
-                  std=[0.229, 0.224, 0.225])
-])
-
-tf_te = tfs.Compose([
-    tfs.Resize(32),
-    tfs.ToTensor(),
-    tfs.Normalize(mean=[0.485, 0.456, 0.406],
-                  std=[0.229, 0.224, 0.225])
-])
 
 
 class CustomCIFAR10(CIFAR10):
@@ -48,29 +29,16 @@ class CustomCIFAR10(CIFAR10):
         img = self.data[idx]
         img = Image.fromarray(img).convert('RGB')
         imgs = [self.transform(img), self.transform(img)]
-        return torch.stack(imgs)
+        return torch.stack(imgs)  # stack a positive pair
 
 
-ds_tr = CustomCIFAR10(root='data', train=True, transform=tf_tr, download=True)
 ds_de = CIFAR10(root='data', train=True, transform=tf_de, download=True)
 ds_te = CIFAR10(root='data', train=False, transform=tf_te, download=True)
 
 
-dl_tr = DataLoader(ds_tr, batch_size=256, shuffle=True)
-dl_de = DataLoader(ds_de, batch_size=256, shuffle=True)
-dl_te = DataLoader(ds_te, batch_size=256, shuffle=False)
-
 model = resnet18(pretrained=False)
 model.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
 model.maxpool = nn.Identity()
-
-
-ch = model.fc.in_features
-model.fc = nn.Sequential(nn.Linear(ch, ch),
-                         nn.ReLU(),
-                         nn.Linear(ch, ch))
-model.to("cuda")
-model = nn.DataParallel(model, device_ids=[0, 1])
 
 
 def pair_cosine_similarity(x, eps=1e-8):
@@ -91,56 +59,109 @@ def nt_xent(x, t=0.5):
     return -torch.log(x.mean())
 
 
-optimizer = Adam(model.parameters(), lr=0.001)
+@hydra.main(config_path='SimCLR_config.yaml')
+def train_SimCLR(args: DictConfig) -> None:
+    assert torch.cuda.is_available()
+    transform = tfs.Compose([
+        tfs.RandomResizedCrop(32),
+        tfs.RandomHorizontalFlip(),
+        tfs.ColorJitter(0.5, 0.5, 0.5, 0.5),
+        tfs.ToTensor(),
+        tfs.Normalize(mean=[0.485, 0.456, 0.406],
+                      std=[0.229, 0.224, 0.225])
+    ])
 
-loss_meter = AverageMeter("SimCLR_loss")
+    train_set = CustomCIFAR10(root=args.data_dir, train=True, transform=transform, download=True)
 
-# SimCLR training
-model.train()
-for epoch in range(100):
-    for data in dl_tr:
-        d = data.size()
-        x = data.view(d[0]*2, d[2], d[3], d[4]).to('cuda')
-        optimizer.zero_grad()
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+
+    model = eval(args.backbone)(pretrained=args.pretrained)
+
+    # use 3x3 rather than 7x7, because size of cifar10 is much smaller than ImageNet
+    model.conv1 = nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+    model.maxpool = nn.Identity()
+
+    mlp_dim = model.fc.in_features
+    model.fc = nn.Sequential(nn.Linear(mlp_dim, mlp_dim),
+                             nn.ReLU(),
+                             nn.Linear(mlp_dim, mlp_dim))
+    model.cuda()
+    model = nn.DataParallel(model, device_ids=[0, 1])
+
+    optimizer = Adam(model.parameters(), lr=0.001)
+    if args.load_checkpoint:
+        save_path = 'cifar10-rn50-mlp-b256-t0.5-e90.pt'
+        model.load_state_dict(torch.load(save_path))
+    else:
+        loss_meter = AverageMeter("SimCLR_loss")
+
+        # SimCLR training
+        model.train()
+        for epoch in range(args.epochs):
+            for x in train_loader:
+                sizes = x.size()
+                x = x.view(sizes[0] * 2, sizes[2], sizes[3], sizes[4]).cuda()
+
+                optimizer.zero_grad()
+                loss = nt_xent(model(x))
+                loss.backward()
+                optimizer.step()
+
+                loss_meter.update(loss.item(), x.size(0))
+
+            logger.info("Epoch {}, SimCLR loss: {:.4f}".format(epoch, loss_meter.avg))
+
+            if (epoch + 1) % args.log_interval == 0:
+                torch.save(model.state_dict(), 'cifar10-rn50-mlp-b256-t0.5-e' + str(epoch + 1) + '.pt')
+
+    #  finetune a linear classifier
+    optimizer = Adam(model.parameters(), lr=0.003)
+    criterion = nn.CrossEntropyLoss()
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    normal_transform = tfs.Compose([
+        tfs.Resize(32),
+        tfs.ToTensor(),
+        tfs.Normalize(mean=[0.485, 0.456, 0.406],
+                      std=[0.229, 0.224, 0.225])
+    ])
+
+    train_set = CIFAR10(root=args.data_dir, train=True, transform=normal_transform, download=True)
+    test_set = CIFAR10(root=args.data_dir, train=False, transform=normal_transform, download=True)
+
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False)
+
+    model.fc = nn.Linear(mlp_dim, len(train_set.classes))
+    model.cuda()
+    model = nn.DataParallel(model, device_ids=[0, 1])
+
+    model.train()
+    classification_loss_meter = AverageMeter("classification_loss")
+    for epoch in range(5):
+        for batch_id, (x, y) in train_loader:
+            x, y = x.cuda(), y.cuda()
+            optimizer.zero_grad()
+            pred = model(x)
+            loss = criterion(pred, y)
+            loss.backward()
+            optimizer.step()
+            classification_loss_meter.update(loss.item(), x.size(0))
+
+    model.eval()
+    acc_meter = AverageMeter("Acc")
+    for batch_id, (x, y) in test_loader:
+        x, y = x.cuda(), y.cuda()
         p = model(x)
-        loss = nt_xent(p)
-        loss.backward()
-        loss_meter.update(loss.item(), x.size(0))
-        optimizer.step()
-    logger.info("Epoch {}, SimCLR loss: {:.4f}".format(epoch, loss_meter.avg))
+        acc = (p.argmax(dim=-1) == y).float().mean().item()
+        acc_meter.update(acc, x.size(0))
 
-    if (epoch + 1) % 10 == 0:
-        torch.save(model.state_dict(), 'cifar10-rn50-mlp-b256-t0.5-e'+str(epoch + 1)+'.pt')
-
-for param in model.parameters():
-    param.requires_grad = False
-
-model.fc = nn.Linear(ch, len(ds_de.classes))
-model.to('cuda')
-model = nn.DataParallel(model, device_ids=[0, 1])
+    logger.info("Test Acc: {:.4f}".format(acc_meter.avg))
 
 
-optimizer = Adam(model.parameters(), lr=0.003)
-criterion = nn.CrossEntropyLoss()
+if __name__ == '__main__':
+    train_SimCLR()
 
 
-model.train()
-for i in range(5):
-    for data in dl_de:
-        x, y = data[0].to('cuda'), data[1].to('cuda')
-        optimizer.zero_grad()
-        p = model(x)
-        loss = criterion(p, y)
-        loss.backward()
-        optimizer.step()
-
-
-model.eval()
-acc_list = []
-for data in dl_te:
-    x, y = data[0].to('cuda'), data[1].to('cuda')
-    p = model(x)
-    acc = (p.argmax(dim=-1) == y).float().mean().item()
-    acc_list.append(acc)
-
-logger.info("Test Acc: {:.4f}".format(np.mean(acc_list)))
