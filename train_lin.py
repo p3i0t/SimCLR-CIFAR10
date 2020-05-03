8c38b9367aefd6929769ab1c1ff6fb05f6ddd675
a-name-not-used <wangxin_0716@me.com>
2020-05-03 18:35:00 +0800

    Initial

--- train_lin.py
+++ train_lin.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-import math
 import os
 
 import torch
 import torch.nn as nn
-from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.backends.cudnn as cudnn
-import torch.distributed as dist
-import torch.optim
-import torch.multiprocessing as mp
-import torch.utils.data
-from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
-
-# import moco.loader
+import torch.nn.functional as F
 from utils import AverageMeter
-import moco.builder
-from PIL import Image
+
 
 import hydra
 from omegaconf import DictConfig
@@ -33,38 +25,8 @@ model_names = sorted(name for name in models.__dict__
     and callable(models.__dict__[name]))
 
 
-@hydra.main(config_path='config_moco.yml')
+@hydra.main(config_path='config_lin.yml')
 def main(args: DictConfig) -> None:
-    """
-    One node (machine), N GPU cards. So world_size=1 is adjusted to world_size * N, the # processes.
-    """
-    n_gpus = torch.cuda.device_count()
-    args.world_size = n_gpus * args.world_size
-    # Use torch.multiprocessing.spawn to launch distributed processes: the main_worker process function
-    mp.spawn(main_worker, nprocs=n_gpus, args=(args, ))
-
-
-class CustomCIFAR10(datasets.CIFAR10):
-    def __init__(self, **kwds):
-        super().__init__(**kwds)
-
-    def __getitem__(self, idx):
-        if not self.train:
-            return super().__getitem__(idx)
-
-        img = self.data[idx]
-        img = Image.fromarray(img).convert('RGB')
-        return self.transform(img), self.transform(img)
-        # imgs = [self.transform(img), self.transform(img)]
-        # return torch.stack(imgs)  # stack a positive pair
-
-
-def main_worker(rank, args):
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '1234'
-
-    dist.init_process_group(backend=args.dist_backend, world_size=args.world_size, rank=rank)
-
     # create model
     logger.info("=> creating model '{}'".format(args.backbone))
     model = models.__dict__[args.backbone]()
@@ -82,7 +44,6 @@ def main_worker(rank, args):
 
     # load pre-trained
     checkpoint = torch.load('checkpoint_{}.pt'.format(args.load_epoch), map_location="cpu")
-
     # rename moco pre-trained keys
     state_dict = checkpoint['state_dict']
     for k in list(state_dict.keys()):
@@ -96,19 +57,17 @@ def main_worker(rank, args):
     msg = model.load_state_dict(state_dict, strict=False)
     assert set(msg.missing_keys) == {"fc.weight", "fc.bias"}
 
-    print("=> loaded pre-trained model checkpoint_{}.pt".format(args.load_epoch))
-    torch.cuda.set_device(rank)  # put the model and training on GPU rank.
+    logger.info("=> loaded pre-trained model checkpoint_{}.pt".format(args.load_epoch))
     model = model.cuda()
-    model = DDP(model, device_ids=[rank], output_device=rank)
 
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda()
-    # optimize only the linear classifier
     parameters = list(filter(lambda p: p.requires_grad, model.parameters()))
     assert len(parameters) == 2  # fc.weight, fc.bias
-    optimizer = torch.optim.SGD(parameters, 0.1,
-                                momentum=args.momentum,
-                                weight_decay=0.)
+    # optimizer = torch.optim.SGD(parameters, 0.1,
+    #                             momentum=args.momentum,
+    #                             weight_decay=0.)
+    optimizer = torch.optim.Adam(parameters, lr=0.01, weight_decay=1e-5)
 
     cudnn.benchmark = True
 
@@ -120,65 +79,60 @@ def main_worker(rank, args):
                              std=[0.229, 0.224, 0.225])
     ])
 
-    # data_dir = hydra.utils.to_absolute_path(args.data_dir)
-    train_dataset = datasets.CIFAR10(root='../../../data', train=True, transform=transform, download=True)
-    train_sampler = DistributedSampler(train_dataset)
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=False,
-        num_workers=16, pin_memory=True, sampler=train_sampler, drop_last=True)
+    transform_ = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
 
+    data_dir = hydra.utils.to_absolute_path(args.data_dir)
+    train_dataset = datasets.CIFAR10(root=data_dir, train=True, transform=transform, download=True)
+    test_dataset = datasets.CIFAR10(root=data_dir, train=False, transform=transform_, download=True)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True,  drop_last=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False,  drop_last=False)
+
+    optimal_loss = 1e5
     for epoch in range(1, args.finetune_epochs + 1):
-        train_sampler.set_epoch(epoch)
-        adjust_learning_rate(optimizer, epoch, args)
-
-        # train for one epoch
-        train(train_loader, model, criterion, optimizer, epoch)
-        if epoch >= 10 and epoch % 10 == 0:
+        loss, acc = run_epoch(model, train_loader, optimizer)
+        logger.info("Epoch {}, train loss: {:.4f}, acc: {:.4f}".format(epoch, loss, acc))
+        if loss < optimal_loss:
+            optimal_loss = loss
+            logger.info("==> New best results")
             checkpoint = {
                 'epoch': epoch,
                 'backbone': args.backbone,
                 'state_dict': model.state_dict(),
-                'optimizer' : optimizer.state_dict(),
+                'optimizer': optimizer.state_dict(),
             }
+            torch.save(checkpoint, "lin_checkpoint_best.pt")
 
-            torch.save(checkpoint, "lin_checkpoint_{}.pt".format(epoch))
+        loss, acc = run_epoch(model, test_loader)
+        logger.info("test loss: {:.4f}, acc: {:.4f}".format(epoch, loss, acc))
 
 
-def train(train_loader, model, criterion, optimizer, epoch):
-    losses = AverageMeter('Loss')
-    acc = AverageMeter('Acc')
+def run_epoch(model, dataloader, optimizer=None):
+    loss_meter = AverageMeter('Loss')
+    acc_meter = AverageMeter('Acc')
 
-    # switch to train mode
-    model.train()
-    for batch_id, (x, y) in enumerate(train_loader):
+    if optimizer:
+        model.train()
+    else:
+        model.eval()
+    for batch_id, (x, y) in enumerate(dataloader):
         x = x.cuda(non_blocking=True)
         y = y.cuda(non_blocking=True)
 
-        # compute output
         logits = model(x)
-        loss = criterion(logits, y)
+        loss = F.cross_entropy(logits, y)
 
-        # acc are (K+1)-way contrast classifier accuracy
-        # measure accuracy and record loss
-        acc1 = (logits.argmax(dim=1) == y).float().mean().item()
-
-        acc.update(acc1, x.size(0))
-        losses.update(loss.item(), x.size(0))
-
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print("Epoch {}, loss: {:.4f}, acc: {:.4f}".format(epoch, losses.avg, acc.avg))
-
-
-def adjust_learning_rate(optimizer, epoch, args):
-    """Decay the learning rate based on schedule"""
-    lr = args.lr
-    for milestone in args.schedule:
-        lr *= 0.1 if epoch >= milestone else 1.
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        acc = (logits.argmax(dim=1) == y).float().mean().item()
+        acc_meter.update(acc, x.size(0))
+        loss_meter.update(loss.item(), x.size(0))
+        if optimizer:
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    return loss_meter.avg, acc_meter.avg
 
 
 if __name__ == '__main__':
