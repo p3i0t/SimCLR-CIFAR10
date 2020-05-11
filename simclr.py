ffd358d874fd7b66c8a33a337640298037358e06
a-name-not-used <wangxin_0716@me.com>
2020-05-12 01:03:25 +0800

    Initial

---
+++ simclr.py
@@ -0,0 +1,136 @@
+import hydra
+from omegaconf import DictConfig
+import logging
+
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.nn as nn
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+
+from models import Model
+from utils import AverageMeter
+from utils_transforms import get_cifar10_transforms
+
+from tqdm import tqdm
+
+
+logger = logging.getLogger(__name__)
+
+
+class CIFAR10Pair(CIFAR10):
+    """Generate mini-batche pairs on CIFAR10 training set."""
+    def __getitem__(self, idx):
+        img, target = self.data[idx], self.targets[idx]
+        img = Image.fromarray(img)  # .convert('RGB')
+        imgs = [self.transform(img), self.transform(img)]
+        return torch.stack(imgs), target  # stack a positive pair
+
+
+def pair_cosine_similarity(x, eps=1e-8):
+    n = x.norm(p=2, dim=1, keepdim=True)
+    return (x @ x.t()) / (n * n.t()).clamp(min=eps)
+
+
+def nt_xent(x, t=0.5):
+    x = pair_cosine_similarity(x)
+    x = torch.exp(x / t)
+    idx = torch.arange(x.size()[0])
+
+    # put positive pairs on the diagonal
+    idx[::2] += 1
+    idx[1::2] -= 1
+    x = x[idx]
+
+    # subtract the similarity of 1 from the numerator
+    x = x.diag() / (x.sum(0) - torch.exp(torch.tensor(1 / t)))
+    return -torch.log(x.mean())
+
+
+def get_lr(step, total_steps, lr_max, lr_min):
+    """Compute learning rate according to cosine annealing schedule."""
+    return lr_min + (lr_max - lr_min) * 0.5 * (1 + np.cos(step / total_steps * np.pi))
+
+
+@hydra.main(config_path='simclr_config.yml')
+def train(args: DictConfig) -> None:
+    assert torch.cuda.is_available()
+    train_transform, test_transform = get_cifar10_transforms(s=0.5)
+
+    data_dir = hydra.utils.to_absolute_path(args.data_dir)  # get absolute path of data dir
+    train_set = CIFAR10Pair(root=data_dir,
+                            train=True,
+                            transform=train_transform,
+                            download=True)
+
+    # train_set_eval = CIFAR10Pair(root=data_dir,
+    #                              train=True,
+    #                              transform=test_transform,
+    #                              download=False)
+
+    train_loader = DataLoader(train_set,
+                              batch_size=args.batch_size,
+                              shuffle=True,
+                              num_workers=args.workers,
+                              drop_last=True)
+
+    # eval_loader = DataLoader(train_set_eval,
+    #                          batch_size=args.batch_size,
+    #                          shuffle=False,
+    #                          num_workers=args.workers,
+    #                          drop_last=True)
+
+    # dataloader bars
+    train_bar = tqdm(train_loader)
+    # eval_bar = tqdm(eval_loader)
+
+    # Prepare model
+    model = Model(projection_dim=args.projection_dim).cuda()
+    logger.info('feature dim: {}, projection dim: {}'.format(model.feature_dim, args.projection_dim))
+
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        args.learning_rate,
+        momentum=args.momentum,
+        weight_decay=args.weight_decay,
+        nesterov=True)
+
+    # cosine annealing lr
+    scheduler = LambdaLR(
+        optimizer,
+        lr_lambda=lambda step: get_lr(  # pylint: disable=g-long-lambda
+            step,
+            args.epochs * len(train_loader),
+            args.learning_rate,  # lr_lambda computes multiplicative factor
+            1e-6))
+
+    # SimCLR training
+    model.train()
+    for epoch in range(1, args.epochs + 1):
+        loss_meter = AverageMeter("SimCLR_loss")
+        for x, y in train_bar:
+            sizes = x.size()
+            x = x.view(sizes[0] * 2, sizes[2], sizes[3], sizes[4]).cuda()
+
+            optimizer.zero_grad()
+            loss = nt_xent(model(x), args.temperature)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            loss_meter.update(loss.item(), x.size(0))
+
+        # save checkpoint very log_interval epochs
+        if epoch >= args.log_interval and epoch % args.log_interval == 0:
+            logger.info("Epoch {}, SimCLR loss: {:.4f}".format(epoch, loss_meter.avg))
+            # Save checkpoint
+            torch.save(model.state_dict(), 'simclr_epoch{}.pt'.format(epoch))
+
+
+if __name__ == '__main__':
+    train()
+
+
